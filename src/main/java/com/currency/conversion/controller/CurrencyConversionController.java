81ba44d0f4bfe8d1910d93b91796364f1c3b9c2c
ROBIN SINGH <robinsingh0210@gmail.com>
2018-12-18 15:50:23 +0530

    Readme added

--- src/main/java/com/currency/conversion/controller/CurrencyConversionController.java
+++ src/main/java/com/currency/conversion/controller/CurrencyConversionController.java
@@ -2,6 +2,7 @@ package com.currency.conversion.controller;
 
 import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import com.currency.conversion.service.CurrencyConvServiceProxy;
 public class CurrencyConversionController {
 	@Autowired
 	private CurrencyConvServiceProxy proxy;
+	@Autowired
+	private Environment environment; 
 
 	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConvBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
